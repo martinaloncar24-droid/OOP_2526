71b8ed5c5c34b96a45a2d78e3ac777e33bfe6ba3
martinaloncar24-droid <martinaloncar24@gmail.com>
2026-03-10 10:05:19 +0100

    class student is defined

--- Fundamentals/src/pckg_cls_obj/AppTest.java
+++ Fundamentals/src/pckg_cls_obj/AppTest.java
@@ -3,9 +3,18 @@ package pckg_cls_obj;
 public class AppTest {
 
     public static void main(String[] args){
-        Student s1 = new Student(name:"Petar", surname:"Petrić", id:"12");
-        Student s2 = new Student(name:"Franka", surname:"Franić", id:"13");
+        Student s1;
+        s1 = new Student( 'Petar', 'Petrić', 12);
+        Student s2 = new Student('Franka', 'Franić', 43);
         s1.subscribeToStudentService();
+        s2.subscribeToStudentService();
+        System.out.println(s1);
+        Hlace h1 = new Hlace("Levis 501");
+        h1.koristeneHlace();
+        Hlace h2;
+        h2 = h1.nekakoOciscene(s1);
+        System.out.println(h1);
+        System.out.println(h2);
 
     }
 }
--- Fundamentals/src/pckg_cls_obj/Student.java
+++ Fundamentals/src/pckg_cls_obj/Student.java
@@ -5,6 +5,22 @@ public class Student {
     private String name;
     private String surname;
     private int id;
+    private static int cntID = 100;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    /*
+    public String (String name, String surname){
+
+        this.id = cntID;
+        cntID++;
+        this.name = name;
+        this.surname = surname;
+    }
+    */
 
     public Student(String name, String surname, int id){
         this.name = name;
@@ -13,7 +29,22 @@ public class Student {
     }
 
     public void subscribeToStudentService(){
-        System.out.println("Student's id:" + id);
-        System.out.println(name + "-" + surname + "is subscribed to student service.");
+        System.out.println("Student's id: " + id);
+        System.out.println(name  + " - " + surname + " is subscribed to student service.");
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public Hlace operiMe(Hlace hlace) {
+        hlace.setCiste(true);
+        System.out.println(this + "je oprao hlače!");
+        return hlace;
     }
 }
